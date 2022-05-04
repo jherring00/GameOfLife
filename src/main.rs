b1332544775d6e67a1f798ec4405edad450f2e2d
Jacob Herring <jacobherring00@gmail.com>
2022-05-04 09:09:39 -0400

    more sane version of scanning all the squares

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use std::{thread,time};
 fn main() {
     let mut grid: Vec<Vec<u8>> = vec![
-        vec![0, 0, 0, 0, 0],
-        vec![1, 1, 1, 0, 0],
-        vec![0, 0, 1, 0, 0],
-        vec![0, 0, 1, 0, 0],
-        vec![0, 0, 0, 0, 0],
+        vec![0, 1, 0],
+        vec![0, 1, 0],
+        vec![0, 1, 0],
     ];
     let ten_millis = time::Duration::from_millis(1000);
     print_board(&grid);
@@ -27,57 +25,17 @@ fn game_of_life(grid: &mut Vec<Vec<u8>>){
         for y in 0..cells {
             //if if the current cell is at the edge of the board then we only check the indeces on the board
             let mut neighbors = 0;
-
-            if x as i8 - 1 < 0 {
-                neighbors += old[x + 1][y];
-                if y as i8 - 1 < 0 {
-                    neighbors += old[x][y + 1];
-                    neighbors += old[x + 1][y + 1];
-                } else if y + 1 == cells {
-                    neighbors += old[x][y - 1];
-                    neighbors += old[x + 1][y - 1];
-                } else {
-                    neighbors += old[x][y - 1];
-                    neighbors += old[x][y + 1];
-
-                    neighbors += old[x + 1][y - 1];
-                    neighbors += old[x + 1][y + 1];
+            for i in -1i8..=1{
+                for j in -1i8..=1{
+                    let new_x = x as i8 + i;
+                    let new_y = y as i8 + j;
+                    if new_x >= 0 && new_y >= 0 && new_x < rows as i8 && new_y < cells as i8 {
+                        neighbors+=old[new_x as usize][new_y as usize];
+                    }
                 }
-            } else if x + 1 == rows {
-                neighbors += old[x - 1][y];
-                if y as i8 - 1 < 0 {
-                    neighbors += old[x][y + 1];
-                    neighbors += old[x - 1][y + 1];
-                } else if y + 1 == cells {
-                    neighbors += old[x][y - 1];
-                    neighbors += old[x - 1][y - 1];
-                } else {
-                    neighbors += old[x][y - 1];
-                    neighbors += old[x][y + 1];
 
-                    neighbors += old[x - 1][y - 1];
-                    neighbors += old[x - 1][y + 1];
-                }
-            } else {
-                neighbors += old[x - 1][y];
-                neighbors += old[x + 1][y];
-                if y as i8 - 1 < 0 {
-                    neighbors += old[x - 1][y + 1];
-                    neighbors += old[x][y + 1];
-                    neighbors += old[x + 1][y + 1];
-                } else if y + 1 == cells {
-                    neighbors += old[x - 1][y - 1];
-                    neighbors += old[x][y - 1];
-                    neighbors += old[x + 1][y - 1];
-                } else {
-                    neighbors += old[x - 1][y - 1];
-                    neighbors += old[x - 1][y + 1];
-                    neighbors += old[x][y - 1];
-                    neighbors += old[x][y + 1];
-                    neighbors += old[x + 1][y - 1];
-                    neighbors += old[x + 1][y + 1];
-                }
             }
+            neighbors-=old[x][y];
             // println!("cell @ {},{} has {} neighbors", x, y, neighbors);
             if old[x][y] == 0 {
                 if neighbors == 3 {
